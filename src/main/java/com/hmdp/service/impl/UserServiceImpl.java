3f7322acfcf9605b260517db35b2dffb202d9c92
suwp <1361075240@qq.com>
2023-02-17 15:53:36 +0800

    Initial commit

---
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package com.hmdp.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.User;
+import com.hmdp.mapper.UserMapper;
+import com.hmdp.service.IUserService;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    @Override
+    public Result getCode(String phone, HttpSession session) {
+        // 验证手机号
+        // 不符合
+        // 符合，生成验证码
+        // 保存验证码到session
+        // 发送验证码
+        return null;
+    }
+}
